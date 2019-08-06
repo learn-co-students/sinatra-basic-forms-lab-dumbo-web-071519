69593737d63691468315160c4566fe2abfe00603
Lilaro <larochelle.lisa@gmail.com>
2019-08-06 17:32:58 -0400

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,11 @@
 class Puppy
 
     attr_accessor :name, :breed, :months_old
-
+    @@puppy = []
     def initialize(name, breed, months_old)
         @name = name 
         @breed = breed
         @months_old = months_old
+        @@puppy << self
     end
 end
